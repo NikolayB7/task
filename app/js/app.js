0be00d3cc3a44cd061c93cdcf5742083a7be2e39
NikolayB7 <biletskiyna@gmail.com>
2021-12-22 10:48:26 +0200

    pages

--- app/js/app.js
+++ app/js/app.js
@@ -1,104 +1,19 @@
-
-
-
 $(function () {
 
-    /**Burger*/
+    $('[data-toggle="tooltip"]').tooltip()
 
-    var btnBurger = $('.burger-mobile');
 
-    btnBurger.on('click', function () {
-        if (!$(this).hasClass('active-mobile-btn')) {
-            $(this).addClass('active-mobile-btn');
-            $('body').addClass('active-menu');
-        } else {
-            $(this).removeClass('active-mobile-btn');
-            $('body').removeClass('active-menu');
-        }
-    });
-
-    $('.overlay-menu').on('click', function () {
-        $('body').removeClass('active-menu');
-        $('.burger-mobile').removeClass('active-mobile-btn');
+    $('.search-btn').on('click', () => {
+        $('.search').addClass('open')
     })
 
-
-    /** Anchor*/
-
-    $("body").on("click", "a[href*='#']", function (event) {
-        event.preventDefault();
-
-        if ($('body').hasClass('active-menu')) {
-            $('body').removeClass('active-menu')
-            $('.burger-mobile').removeClass('active-mobile-btn')
-
+    $(document).mouseup(function (e){
+        var div = $(".search");
+        if (!div.is(e.target)
+            && div.has(e.target).length === 0) {
+            div.removeClass('open');
         }
-
-
-        //забираем идентификатор бока с атрибута href
-        var id = $(this).attr('href');
-        //Высота шапкис учетом отступов если она фиксированая (если нет убрать переменную heightHeader с top )
-        var heightHeader = $('.header').outerHeight(true);
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-       var  top = $(id).offset().top - heightHeader; //Если header fixed
-        // var top = $('#' + id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({
-            scrollTop: top
-        }, 800);
     });
 
-    /** Adaptive */
-    var window_width = window.innerWidth || document.documentElement.clientWidth,
-        lgMax = 1199,
-        lgMin = 992,
-        mdMax = 991,
-        mdMin = 768,
-        smMin = 576,
-        xsMax = 575;
 
-    $(window).resize(function () {
-        window_width = window_width = window.innerWidth || document.documentElement.clientWidth;
-        changeMenu();
-    });
-
-    var pageAside = $('.page-aside'),
-        authButton = $('#auth-header'),
-        headerLang = $('.header-language'),
-        headerMenu = $('#nav-header'),
-        headerPhone = $('.header-phones'),
-        siteSearch = $('#header-search-form');
-
-
-    function desctopMenu() {
-        $('#header-checkout-sidebar').before(authButton);
-        $('#page-header .left-header').append(headerLang);
-        $('#page-header .left-header').after(headerMenu);
-        headerMenu.after(headerPhone);
-        pageAside.prepend(siteSearch);
-    }
-
-    function mobileMenu() {
-        pageAside.append(authButton);
-        pageAside.append(headerLang);
-        pageAside.append(headerMenu);
-        pageAside.append(headerPhone);
-        $('#page-header').append(siteSearch);
-    }
-
-    function changeMenu() {
-        if (window_width <= lgMax && !$('body').hasClass('mobile-ui')) {
-            $('body').addClass('mobile-ui');
-            $('body').removeClass('desktop-ui');
-            mobileMenu();
-        } else {
-            if ($(window).outerWidth() > lgMax && !$('body').hasClass('desktop-ui')) {
-                $('body').addClass('desktop-ui');
-                $('body').removeClass('mobile-ui');
-                desctopMenu();
-            }
-        }
-    }
-
-    changeMenu();
 });
